4f60b0f1138788200dec484ced8861183d7af443
Joyce Zhang <joycezhang2065@gmail.com>
2022-07-27 15:53:45 -0400

    implemented dfs to find bodies of water on map

--- My2DGame/src/main/AssetSetter.java
+++ My2DGame/src/main/AssetSetter.java
@@ -12,13 +12,53 @@ import object.OBJ_Fish;
 public class AssetSetter {
 	GamePanel gp;
 	Graphics2D g2;
+	public int itemNum = 0;
 	
 	public AssetSetter(GamePanel gp){
 		this.gp = gp;
 	}
 	
 	public void setObject() {
-		int itemNum = 0;
+		setFish(itemNum);
+		setBag(itemNum);
+		findWater();
+	}
+	
+	public void findWater() {
+		int numWater = 0;
+		int[][] visited = new int[gp.maxWorldRow][gp.maxWorldCol];
+		
+		
+		
+		// FIND TOTAL BODIES OF WATER ON MAP
+		for(int i = 0; i < gp.maxWorldRow; i++) {
+			for(int j = 0; j < gp.maxWorldCol; j++) {
+				if(gp.tileM.mapTileNum[i][j] == 2 && visited[i][j] == 0) {
+					numWater += 1;
+					dfs(gp.tileM.mapTileNum, visited, i, j);
+					
+				}
+			}
+		}
+		
+		System.out.println(numWater);
+		
+	}
+	
+	public void dfs(int mapTileNum[][], int visited[][], int i, int j) {
+		if(i > gp.maxWorldRow || i < 0 || j > gp.maxWorldCol || j < 0) return;
+		if(visited[i][j] == 1 || mapTileNum[i][j] != 2) return;
+		
+		visited[i][j] = 1;
+		
+		dfs(mapTileNum, visited, i-1, j);
+		dfs(mapTileNum, visited, i+1, j);
+		dfs(mapTileNum, visited, i, j-1);
+		dfs(mapTileNum, visited, i, j+1);
+		
+	}
+	
+	public void setFish(int itemNum) {
 		int landFish = 0;
 		boolean fishCap = false;
 		
@@ -46,7 +86,9 @@ public class AssetSetter {
 				}
 			}
 		}
-		
+	}
+	
+	public void setBag(int itemNum) {
 		// SET OTHER ITEMS
 		gp.obj[itemNum] = new OBJ_Bag(gp);
 		gp.obj[itemNum].worldX = gp.tileSize * 9 + 8;
